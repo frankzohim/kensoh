4e5589a046d9daee6b51c59ecde388c863eba21b
frankzohim <delanofofe@gmail.com>
2022-02-16 14:26:51 +0200

    commit from monaco

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -7,15 +7,15 @@
         <div class="slide-1 home-slider">
             <div>
                 <div class="home  text-center">
-                    <img src="../assets/images/home-banner/1.jpg" alt="" class="bg-img blur-up lazyload">
+                    <img src="/assets/images/home-banner/1.jpg" alt="" class="bg-img blur-up lazyload">
                     <div class="container">
                         <div class="row">
                             <div class="col">
                                 <div class="slider-contain">
                                     <div>
-                                        <h4>welcome to fashion</h4>
-                                        <h1>men fashion</h1>
-                                        <a href="#" class="btn btn-solid">shop now</a>
+                                        <h4 style="color:white;">Bienvenu(e) sur Kensoh</h4>
+                                        <h1 style="color:white;">Logistics & Transport</h1>
+                                        <a href="#" class="btn btn-solid">En Savoir Plus</a>
                                     </div>
                                 </div>
                             </div>
@@ -23,25 +23,6 @@
                     </div>
                 </div>
             </div>
-            <div>
-                <div class="home text-center">
-                    <img src="../assets/images/home-banner/2.jpg" alt="" class="bg-img blur-up lazyload">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col">
-                                <div class="slider-contain">
-                                    <div>
-                                        <h4>welcome to fashion</h4>
-                                        <h1>women fashion</h1>
-                                        <a href="#" class="btn btn-solid">shop now</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
     </section>
     <!-- Home slider end -->
 @endsection

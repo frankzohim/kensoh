f3658576fc37ef71275b43142a30f403a405e3f9
Frank  Zohim <69459460+frankzohim@users.noreply.github.com>
2022-02-18 14:24:49 +0200

    commit from monaco

--- resources/views/layouts/frontoffice/header.blade.php
+++ resources/views/layouts/frontoffice/header.blade.php
@@ -441,45 +441,7 @@
                                                 </ul>
                                             </div>
                                         </li>
-                                        <li class="onhover-div mobile-cart">
-                                            <div><img src="../assets/images/icon/cart.png" class="img-fluid blur-up lazyload" alt=""> <i class="ti-shopping-cart"></i></div>
-                                            <span class="cart_qty_cls">2</span>
-                                            <ul class="show-div shopping-cart">
-                                                <li>
-                                                    <div class="media">
-                                                        <a href="#"><img alt="" class="me-3" src="../assets/images/fashion/product/1.jpg"></a>
-                                                        <div class="media-body">
-                                                            <a href="#">
-                                                                <h4>item name</h4>
-                                                            </a>
-                                                            <h4><span>1 x $ 299.00</span></h4>
-                                                        </div>
-                                                    </div>
-                                                    <div class="close-circle"><a href="#"><i class="fa fa-times" aria-hidden="true"></i></a></div>
-                                                </li>
-                                                <li>
-                                                    <div class="media">
-                                                        <a href="#"><img alt="" class="me-3" src="../assets/images/fashion/product/2.jpg"></a>
-                                                        <div class="media-body">
-                                                            <a href="#">
-                                                                <h4>item name</h4>
-                                                            </a>
-                                                            <h4><span>1 x $ 299.00</span></h4>
-                                                        </div>
-                                                    </div>
-                                                    <div class="close-circle"><a href="#"><i class="fa fa-times" aria-hidden="true"></i></a></div>
-                                                </li>
-                                                <li>
-                                                    <div class="total">
-                                                        <h5>subtotal : <span>$299.00</span></h5>
-                                                    </div>
-                                                </li>
-                                                <li>
-                                                    <div class="buttons"><a href="cart.html.htm" class="view-cart">view
-                                                            cart</a> <a href="#" class="checkout">checkout</a></div>
-                                                </li>
-                                            </ul>
-                                        </li>
+                                     
                                     </ul>
                                 </div>
                             </div>

49b40cf250c9c5c875458ccf4e34ce1a4eaa8da8
Frank  Zohim <69459460+frankzohim@users.noreply.github.com>
2022-03-03 12:55:53 +0200

    commit from monaco Voyager installed

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});

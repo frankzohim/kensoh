1e4a6a9dfd26c60025dab3391cf25fb2ae631926
Frank  Zohim <69459460+frankzohim@users.noreply.github.com>
2022-03-04 17:31:17 +0200

    Commit from monaco : Authentication add

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('homepage');
-})->name('homepage');
+    return view('welcome');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->name('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::group(['prefix' => 'admin'], function () {
-    Voyager::routes();
-});
+require __DIR__.'/auth.php';

e4416b2f96d1c2ca2c4c49ca7236a050fcd1bd5c
Frank  Zohim <69459460+frankzohim@users.noreply.github.com>
2022-04-08 17:55:14 +0200

    commit from monaco

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -51,14 +51,14 @@
                             <div class="img-wrapper">
                                 <div class="lable-block">@if($product->new == 1)<span class="lable3"> Neuf </span>@endif @if($product->new == 0)<span class="lable4"> Occassion </span>@endif</div>
                                 <div class="front">
-                                    <a href="product-page(no-sidebar).html.htm">
+                                    <a href="#">
                                         @if(count($product->product_images) != 0)
                                         <img src="{{route('product.displayImage',$product->product_images[0]->id)}}" class="img-fluid blur-up lazyload bg-img" alt=""></a>
                                        @endif
                                 </div>
                                 <div class="back">
                                     @if(count($product->product_images) == 2)
-                                        <a href="product-page(no-sidebar).html.htm"><img src="{{route('product.displayImage',$product->product_images[1]->id)}}" class="img-fluid blur-up lazyload bg-img" alt=""></a>
+                                        <a href="#"><img src="{{route('product.displayImage',$product->product_images[1]->id)}}" class="img-fluid blur-up lazyload bg-img" alt=""></a>
                                     @endif
                                 </div>
                                 <div class="cart-info cart-wrap">
@@ -75,7 +75,7 @@
                             <div class="product-detail">
                                 <div class="rating"><i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i>
                                 </div>
-                                <a href="product-page(no-sidebar).html.htm">
+                                <a href="#">
                                     <h6>{{$product->name}}</h6>
                                 </a>
                                 <h4>

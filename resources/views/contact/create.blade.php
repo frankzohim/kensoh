fb05878f50fe86b437d8c3dc66706d7e3cd61182
level237 <Bramslevel129@gmail.com>
2022-04-19 13:58:56 +0100

    init content:add form

--- resources/views/contact/create.blade.php
+++ resources/views/contact/create.blade.php
@@ -3,4 +3,24 @@
 
 @section('content')
 
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="card">
+                <div class="card-header">
+                    <h5>Contact</h5>
+                </div>
+                <div class="card-body">
+                    <form class="row g-3" action="{{ route('contact.store') }}" method="post">
+                    @csrf
+                    <div class="col-md-6">
+                        <label for="exampleInputName" class="form-label">Nom Produit</label>
+                        <input type="text" class="form-control" id="exampleInputName" aria-describedby="NameHelp" value="{{old('name')}}" name="name">
+                    </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection

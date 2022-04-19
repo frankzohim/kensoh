afd34c94adf9b0cb29cd27c29eb45f177a89d323
level237 <Bramslevel129@gmail.com>
2022-04-19 12:59:16 +0100

    init fillable:model Contact

--- app/Models/contact.php
+++ app/Models/contact.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class contact extends Model
 {
     use HasFactory;
+
+    protected $fillable=[
+        'name',
+        'email',
+        'phone',
+        'subject',
+        'localization'
+    ];
 }

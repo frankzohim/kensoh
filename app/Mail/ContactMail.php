13b4696f83c1d85fba82c23c992d38cb8e037f18
level237 <Bramslevel129@gmail.com>
2022-04-20 14:06:26 +0100

    insert constructor

--- app/Mail/ContactMail.php
+++ app/Mail/ContactMail.php
@@ -2,6 +2,7 @@
 
 namespace App\Mail;
 
+use App\Models\contact;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
@@ -16,7 +17,7 @@ class ContactMail extends Mailable
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(contact $contactUser)
     {
         //
     }

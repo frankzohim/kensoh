2c7df9de73f26bec3f5bbb76a6bcea92e93edef2
level237 <Bramslevel129@gmail.com>
2022-04-21 08:59:40 +0100

    email body:package lenght

--- resources/views/mail/send-package-mail.blade.php
+++ resources/views/mail/send-package-mail.blade.php
@@ -1,7 +1,8 @@
 @component('mail::message')
 # Information du colis
 
-Description:{{ $datapackage['description'] }}
+Description:{{ $datapackage['description'] }},
+lenght: {{ $datapackage['lenght'] }}
 
 @component('mail::button', ['url' => ''])
 Button Text

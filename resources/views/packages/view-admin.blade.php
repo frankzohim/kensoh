5ff80b41a51431aad15109696c46fdf7e1042d75
level237 <Bramslevel129@gmail.com>
2022-04-22 16:48:17 +0100

    Adding container-fluid

--- resources/views/packages/view-admin.blade.php
+++ resources/views/packages/view-admin.blade.php
@@ -3,3 +3,26 @@
 @section('title',__('Liste des colis'))
 
 @section('content')
+
+<!-- Container-fluid starts-->
+    <div class="container-fluid">
+        <div class="page-header">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="page-header-left">
+                        <h3>Tous les produits
+                            <small>Tableau de bord Kensoh</small>
+                        </h3>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <ol class="breadcrumb pull-right">
+                        <li class="breadcrumb-item"><a href="{{route('dashboard')}}"><i data-feather="home"></i></a></li>
+                        <li class="breadcrumb-item"><a href="{{route('product.index')}}">Produits</a></li>
+                        <li class="breadcrumb-item active">Liste</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Container-fluid Ends-->

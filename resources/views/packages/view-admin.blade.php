ec2e4e10c4ead2da6dbe3f3310f537a5d60540fd
level237 <Bramslevel129@gmail.com>
2022-04-22 16:55:00 +0100

    Adding content

--- resources/views/packages/view-admin.blade.php
+++ resources/views/packages/view-admin.blade.php
@@ -4,6 +4,8 @@
 
 @section('content')
 
+<div class="page-body">
+
 <!-- Container-fluid starts-->
     <div class="container-fluid">
         <div class="page-header">
@@ -26,3 +28,133 @@
         </div>
     </div>
     <!-- Container-fluid Ends-->
+
+    	<!-- Container-fluid starts-->
+        <div class="container-fluid">
+            <div class="card">
+                <div class="card-header">
+                    <h5>Tous les produits</h5><br>
+                    @if (session('update_success'))
+                                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                                        <span class="alert-icon"><i class="ni ni-like-2"></i></span>
+                                        <span class="alert-text"><strong>Succès! </strong> <strong>{{ session('update_success') }} </strong></span>
+                                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                @endif
+                                @if (session('update_failure'))
+                                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                        <span class="alert-icon"><i class="ni ni-fat-remove"></i></span>
+                                        <span class="alert-text"><strong>Danger!</strong> <strong> {{ session('update_failure') }} </strong> </span>
+                                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                    </div>
+                                @endif
+                </div>
+
+                <div class="card-body vendor-table">
+                <div class="btn-popup pull-right">
+                    <a href='{{route("product.create")}}'><button type="button" class="btn btn-primary">Ajouter un produit</button></a>
+                </div>
+                    <table class="display" id="basic-1">
+                        <thead>
+                        <tr>
+                            <th>Nom</th>
+                            <th>Etat</th>
+                            <th>Prix</th>
+                            <th>Stock</th>
+                            <th>Catégorie</th>
+                            <th>Marque</th>
+                            <th>Boutique</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($products as $product)
+                            <tr>
+                                <td>
+                                    <div class="d-flex vendor-list">
+
+                                        <span>{{$product->name}}</span>
+                                    </div>
+                                </td>
+                                <td>
+                                    @switch($product->new)
+                                        @case(1)
+                                          Neuf
+                                          @break
+                                        @case(0)
+                                            Occassion
+                                        @break
+                                    @endswitch
+                                </td>
+                                <td>
+                                    {{$product->unit_price}}
+                                </td>
+                                <td>
+                                    {{$product->stock_quantity}}
+                                </td>
+                                <td>
+                                    @foreach($categories as $category)
+                                        @if($category->id==$product->category_id)
+                                            {{$category->name}}
+                                        @endif
+                                    @endforeach
+                                </td>
+                                <td>
+                                    @foreach($brands as $brand)
+                                        @if($brand->id==$product->brand_id)
+                                            {{$brand->name}}
+                                        @endif
+                                    @endforeach
+                                </td>
+                                <td>
+                                    @foreach($stores as $store)
+                                        @if($store->id==$product->store_id)
+                                            {{$store->name}}
+                                        @endif
+                                    @endforeach
+                                </td>
+                                <td>
+                                    <div>
+                                    <a href="{{route('product.edit',$product->id)}}" ><i class="fa fa-edit me-2 font-success"></i></a>
+
+                                    <a href="{{ route('product.destroy',['product' => $product->id]) }}" data-bs-toggle="modal" data-original-title="test" data-bs-target="#exampleModal{{$product->id}}"><i class="fa fa-trash font-danger"></i></a>
+
+                                    <div class="modal fade" id="exampleModal{{$product->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title f-w-600" id="exampleModalLabel">Suppression</h5>
+                                                <button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <form method="POST" action="{{ route('product.destroy',['product' => $product->id]) }}" id="delete-form{{$product->id}}">
+                                                @csrf
+                                                <p>{{ __('Voulez vous supprimer cet élément?') }}</p>
+                                                @method('DELETE')
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="submit" class="btn btn-primary">Oui</button>
+                                                <button class="btn btn-secondary" type="button" data-bs-dismiss="modal">Annuler</button>
+                                            </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+        <!-- Container-fluid Ends-->
+
+</div>

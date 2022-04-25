e954bd589715814d71350cce1ebd21a7d1474c37
level237 <Bramslevel129@gmail.com>
2022-04-25 17:28:11 +0100

    import class:controller

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ use App\Http\Controllers\CouponController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\HomePageController;
 use App\Http\Controllers\PackageController;
+use App\Http\Controllers\SearchController;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 
 /*

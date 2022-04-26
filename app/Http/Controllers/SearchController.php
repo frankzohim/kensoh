3b9e617aa6a6fb92cad000b6565f6ae969265b45
level237 <Bramslevel129@gmail.com>
2022-04-26 10:37:33 +0100

    Method which compares the data of a user to that of the product table of the meta_keywords fields

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,11 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class SearchController extends Controller
 {
     public function find(Request $request){
 
-        return view('Search.results');
+        if (isset($request->query)) {
+
+            $query=$request->query;
+            $products=Product::where('meta_keywords','LIKE','%'.$query.'%')->paginate(2);
+
+            return view('Search.results',compact('products'));
+        }
+        else{
+            return view('homepage');
+        }
+        
     }
 }
--- resources/views/Search/results.blade.php
+++ resources/views/Search/results.blade.php
@@ -4,4 +4,11 @@
 
 @section('content')
 
+<table class="table table-light">
+    <tbody>
+        <tr>
+            <td></td>
+        </tr>
+    </tbody>
+</table>
 @endsection

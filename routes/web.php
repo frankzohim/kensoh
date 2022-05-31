2a170677b3f0c7fdbd6ece6f39fc4ce4ba7bdc5f
Mabranche <ngansopangegabrielle@gmail.com>
2022-05-31 14:58:50 +0100

    --- Auto Git Commit ---

--- routes/web.php
+++ routes/web.php
@@ -30,100 +30,100 @@ use Illuminate\Foundation\Auth\EmailVerificationRequest;
 |
 */
 
-	Route::get('/', [homepageController::class,'index'])->name('homepage');
+Route::get('/', [homepageController::class, 'index'])->name('homepage');
 
-	Route::get('about-us', [HomePageController::class,'about'])->name('about');
+Route::get('about-us', [HomePageController::class, 'about'])->name('about');
 
-	Route::get('become-vendor', [HomePageController::class,'vendor'])->name('vendor');
+Route::get('become-vendor', [HomePageController::class, 'vendor'])->name('vendor');
 
-	//route to access shop page
-	Route::get('shop', [ShopController::class, 'index'])
-                ->name('shop');
+//route to access shop page
+Route::get('shop', [ShopController::class, 'index'])
+    ->name('shop');
 
-	Route::get('/backend', function () {
+Route::get('/backend', function () {
 
-		if (Auth::check()) {
-			return view('dashboard');
-		}
-		else
-			return view('backend/login');
-	})->name('backend');
+    if (Auth::check()) {
+        return view('dashboard');
+    } else
+        return view('backend/login');
+})->name('backend');
 
-	//Email verification notice
-	Route::get('/email/verify', function () {
-		return view('auth.verify-email');
-	})->middleware('auth')->name('verification.notice');
+//Email verification notice
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
 
-	//when user click on activate my account on mail to verify his email address
-	Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
-		$request->fulfill();
+//when user click on activate my account on mail to verify his email address
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
 
-		return redirect('/dashboard');
-	})->middleware(['auth', 'signed'])->name('verification.verify');
+    return redirect('/dashboard');
+})->middleware(['auth', 'signed'])->name('verification.verify');
 
-	//Resending Email verification
-	Route::post('/email/verification-notification', function (Request $request) {
-		$request->user()->sendEmailVerificationNotification();
+//Resending Email verification
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
 
-		return back()->with('message', 'Verification link sent!');
-	})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
-	//Route to display product's image
-	Route::get('product/displayImage{id}', [ProductController::class, 'displayImage'])
-                ->name('product.displayImage');
+//Route to display product's image
+Route::get('product/displayImage{id}', [ProductController::class, 'displayImage'])
+    ->name('product.displayImage');
 
-	//Route to delete product's image
-	Route::post('product/destroyImage{id}', [ProductController::class, 'destroyImage'])
-                ->name('product.destroyImage');
+//Route to delete product's image
+Route::post('product/destroyImage{id}', [ProductController::class, 'destroyImage'])
+    ->name('product.destroyImage');
 
-	//Route to product's details
-	Route::get('product/details{id}', [ProductController::class, 'details'])
-                ->name('product.details');
+//Route to product's details
+Route::get('product/details{id}', [ProductController::class, 'details'])
+    ->name('product.details');
 
-	Route::group(['middleware' => ['auth','verified']], function () {
+Route::group(['middleware' => ['auth', 'verified']], function () {
 
-	Route::get('/dashboard', [DashboardController::class, 'index'])
-                ->name('dashboard');
+    Route::get('/dashboard', [DashboardController::class, 'index'])
+        ->name('dashboard');
 
-	Route::get('store/displayImage{id}', [StoreController::class, 'displayImage'])
-                ->name('store.displayImage');
+    Route::get('store/displayImage{id}', [StoreController::class, 'displayImage'])
+        ->name('store.displayImage');
 
-	Route::get('blog/displayImage{id}', [BlogController::class, 'displayImage'])
-                ->name('blog.displayImage');
-
+    Route::get('blog/displayImage{id}', [BlogController::class, 'displayImage'])
+        ->name('blog.displayImage');
 
 
 
-		//Route to save product's images
-		Route::post('product/image', [ProductController::class, 'images'])
-                ->name('product.image');
 
-		//Route test
-		Route::get('test', [ProductController::class, 'test'])
-                ->name('test');
+    //Route to save product's images
+    Route::post('product/image', [ProductController::class, 'images'])
+        ->name('product.image');
 
-		Route::resources([
-			'brand' => BrandController::class,
-			'store' => StoreController::class,
-			'category' => CategoryController::class,
-			'product' => ProductController::class,
-			'coupon' => CouponController::class,
-			'review' => ReviewController::class,
-      'packages'=>PackageController::class,
-			'user' => UserController::class,
-			'town' => TownController::class,
-			'blog' => BlogController::class,
-		]);
-	});
+    //Route test
+    Route::get('test', [ProductController::class, 'test'])
+        ->name('test');
 
-    Route::get('contact',[ContactController::class,'create'])->name('contact.create');
-    Route::post('contact',[ContactController::class,'store'])->name('contact.store');
-	Route::get('search/',[SearchController::class,'find'])->name('search.find');
+    Route::resources([
+        'brand' => BrandController::class,
+        'store' => StoreController::class,
+        'category' => CategoryController::class,
+        'product' => ProductController::class,
+        'coupon' => CouponController::class,
+        'review' => ReviewController::class,
+        'packages' => PackageController::class,
+        'user' => UserController::class,
+        'town' => TownController::class,
+        'blog' => BlogController::class,
+        'faq' => FaqController::class,
+    ]);
+});
+
+Route::get('contact', [ContactController::class, 'create'])->name('contact.create');
+Route::post('contact', [ContactController::class, 'store'])->name('contact.store');
+Route::get('search/', [SearchController::class, 'find'])->name('search.find');
 
 
 
-	Route::group(['prefix' => 'admin'], function () {
-		Voyager::routes();
-	});
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
 
-	require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';

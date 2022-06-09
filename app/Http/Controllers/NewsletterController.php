fa5c7bb1885d0cf0146e035bb4df37d5f848138e
level237 <Bramslevel129@gmail.com>
2022-06-09 12:43:43 +0200

    Subscribe an email on Newsletter

--- app/Http/Controllers/NewsletterController.php
+++ app/Http/Controllers/NewsletterController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Newsletter;
 
 class NewsletterController extends Controller
 {
-    //
+    public function store(Request $request){
+
+        if(! Newsletter::isSubscribed($request->email)){
+            Newsletter::subscribePending($request->email);
+
+            return redirect('homepage')->with('success', 'Thanks For Subscribe');
+        }
+        return redirect('newsletter')->with('failure', 'Sorry! You have already subscribed ');
+    }
 }

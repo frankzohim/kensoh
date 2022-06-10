97b706e0c4fc91e9254503a0a23c0cbc402de2d8
level237 <Bramslevel129@gmail.com>
2022-06-10 18:12:12 +0200

    update Controllers

--- resources/views/shop/shop.blade.php
+++ resources/views/shop/shop.blade.php
@@ -115,7 +115,7 @@
                                                             <div class="product-detail">
                                                                 <div>
                                                                     <div class="rating"><i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i></div>
-                                                                    <a href="product-page(no-sidebar).html.htm">
+                                                                    <a href="{{ route('product.showbyId',$product->id) }}">
                                                                         <h6>{{ $product->name }}</h6>
                                                                     </a>
                                                                     <p>{{ $product->meta_description }}</p>

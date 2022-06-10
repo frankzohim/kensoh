eb3bdd909dfaec07bc290b232f0ca935c6598e13
level237 <Bramslevel129@gmail.com>
2022-06-10 10:45:58 +0200

    update

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\ProductCategory;
+use App\Models\Product;
 
 class ShopController extends Controller
 {
     public function index(){
 
         $categories=ProductCategory::all();
+        $products=Product::select('name','slug')->paginate(8);
 
-        return view('shop/shop',compact('categories'));
+        return view('shop/shop',compact('categories','products'));
     }
 }

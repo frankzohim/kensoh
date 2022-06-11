0f26c87bc0b96295417a21a440059f8bc458ef94
level237 <Bramslevel129@gmail.com>
2022-06-11 10:53:59 +0200

    kensoh logo:update

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -2,7 +2,7 @@
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                <img src="/assets/frontend/images/icon/logo.png" class="img-fluid blur-up lazyload" alt="">
             </a>
         </x-slot>
 

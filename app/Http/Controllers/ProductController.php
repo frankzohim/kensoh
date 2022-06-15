07dc30e08d08417297115112282b53e37ad2a052
level237 <Bramslevel129@gmail.com>
2022-06-15 16:13:06 +0200

    update implementation

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,6 +9,7 @@ use App\Models\Brand;
 use App\Models\Store;
 use App\Models\Review;
 use App\Models\user;
+use App\Models\product_favorite;
 use Illuminate\Http\Request;
 use App\Http\Requests\ProductRequest;
 use Illuminate\Support\Collection;
@@ -298,5 +299,9 @@ class ProductController extends Controller
         $text=preg_replace('~-+~','-',$text);
         $text=strtolower($text);
         return $text;
+    }
+
+    function favorite_product($id){
+        $productfavorite=new product_favorite;
     }
 }

ccc15a40bc4d6f514629c9b5a867e18ddfd9391d
level237 <Bramslevel129@gmail.com>
2022-06-16 14:42:26 +0200

    composant livewire:store

--- app/Http/Livewire/ProductFovorite.php
+++ app/Http/Livewire/ProductFovorite.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\product_favorite;
 use Livewire\Component;
 
 class ProductFovorite extends Component
@@ -9,7 +10,12 @@ class ProductFovorite extends Component
 
     public $product_favorite;
     public $state=[];
+    public $updateMode= false;
 
+    public function store(){
+
+        product_favorite::create($this->state);
+    }
 
     public function render()
     {

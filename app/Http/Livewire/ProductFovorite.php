bb8f607b36f8a11d5be81a59e61a3386875e9698
level237 <Bramslevel129@gmail.com>
2022-06-16 17:02:58 +0200

    mount:init

--- app/Http/Livewire/ProductFovorite.php
+++ app/Http/Livewire/ProductFovorite.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use App\Models\product_favorite;
+use App\Models\Product;
 use Livewire\Component;
 
 class ProductFovorite extends Component
@@ -12,20 +13,20 @@ class ProductFovorite extends Component
     public $state=[];
     public $updateMode= false;
 
-    public function store($user_id,$product_id,){
+    public function store($user_id,$product_id){
         $this->state=[
             $user_id,
             $product_id,
             '1'
         ];
         //product_favorite::add();
-        product_favorite::create($this->state);
+        product_favorite::add($this->state)->associate('App\Models\product');
         $this->reset('state');
         $this->product_favorite = product_favorite::all();
     }
-    public function mount()
+    public function mount(Product $product)
     {
-        $this->product_favorite = product_favorite::all();
+        $this->product_=$product;
     }
 
     public function render()
--- resources/views/livewire/product-fovorite.blade.php
+++ resources/views/livewire/product-fovorite.blade.php
@@ -1,4 +1,4 @@
 <div>
-    <a href="javascript:void(0)" wire:click.prevent="store" title="Add to Wishlist"><i class="fa fa-heart" aria-hidden="true"></i></a>
+
 
 </div>

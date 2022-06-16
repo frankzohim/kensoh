c391bb1630f9cebec77b7b3404561f189be2341f
level237 <Bramslevel129@gmail.com>
2022-06-16 14:17:42 +0200

    composant livewire:init

---
+++ app/Http/Livewire/ProductFovorite.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ProductFovorite extends Component
+{
+    public function render()
+    {
+        return view('livewire.product-fovorite');
+    }
+}

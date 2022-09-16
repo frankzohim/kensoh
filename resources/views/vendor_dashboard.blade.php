632e8503c2e76a4b5b61fe158c954f17de625403
level237 <Bramslevel129@gmail.com>
2022-09-16 13:10:22 -0400

    update implementation store

--- resources/views/vendor_dashboard.blade.php
+++ resources/views/vendor_dashboard.blade.php
@@ -27,6 +27,15 @@
 
 	<!--  dashboard section start -->
     <section class="dashboard-section section-b-space">
+        @if (session('update_success'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <span class="alert-icon"><i class="ni ni-like-2"></i></span>
+            <span class="alert-text"><strong>Succès! </strong> <strong>{{ session('update_success') }} </strong></span>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    @endif
         <div class="container">
             <div class="row">
                 <div class="col-lg-3">
